4587eba73d47a0019b720915a1ab63c0676f9b5e
AlinaMertina <113964973+AlinaMertina@users.noreply.github.com>
2022-12-04 07:55:39 +0430

    Add files via upload

---
+++ EcouterListeChoix.java
@@ -0,0 +1,36 @@
+package ecouteur;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import image.*;
+import java.util.TimerTask;
+import java.util.Timer;
+import client.*;
+
+public class EcouterListeChoix implements MouseListener {
+    ShowListe frame;
+    public void setFrame(ShowListe fr){
+        frame=fr;
+    }
+    public ShowListe getFrame(){
+        return frame;
+    }
+    public EcouterListeChoix(ShowListe fr ){
+        setFrame(fr);
+    }
+    public void mouseClicked(MouseEvent e){
+        for(int i=0;i<frame.getButton().size();i++){
+            if(e.getSource()==frame.getButton().get(i)){
+                frame.getFrame().getEnvoiMessage().setMessage(frame.getButton().get(i).getText());
+            }  
+        }
+    }
+    public void mouseEntered(MouseEvent e){}
+    public void mouseExited(MouseEvent e){}
+    public void mousePressed(MouseEvent e){}
+    public void mouseReleased(MouseEvent e){}
+
+}
